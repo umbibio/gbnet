bd0c148f8878e345e8df1bcde7e7dd74d70fc9c2
Argenis Arriojas <arriojasmaldonado001@umb.edu>
2018-11-01 15:33:34 -0400

    Turn Node+RandomVariable into RandomVariableNode

--- nodes.py
+++ nodes.py
@@ -2,13 +2,24 @@ import numpy as np
 import scipy.stats as st
 
 
-class Node(object):
-    __slots__ = ['id', 'name', 'uid', 'parents', 'children', 'blanket', 'in_edges']
+class RandomVariableNode(object):
 
-    def __init__(self, name, uid):
+
+    __slots__ = [
+        'id', 'name', 'uid', 'parents', 'children', 'blanket',
+        'in_edges', 'value', 'total_sampled', 'dist', 'params']
+
+    
+    def __init__(self, name, uid, **kwargs):
         self.id = f"{name}__{uid}"
         self.name = name
         self.uid = uid
+
+        try:
+            self.value = kwargs['value']
+        except KeyError:
+            self.value = self.rvs()
+        self.total_sampled = 0
         self.parents = []
         self.children = []
         self.blanket = []
@@ -18,31 +29,84 @@ class Node(object):
         # For example an interaction X->Y will have
         # corresponding T and S nodes associated only to X
         self.in_edges = []
-        
 
-class RandomVariable(Node):
-    __slots__ = ['value', 'total_sampled', 'dist', 'params']
-
-    def __init__(self, *args, **kwargs):
-        #self.prior = self.dist(*self.params)
-        try:
-            self.value = kwargs['value']
-        except KeyError:
-            self.value = self.rvs()
-        self.total_sampled = 0
-        Node.__init__(self, *args)
-
+    
     def rvs(self):
         return self.dist.rvs(*self.params) # pylint: disable=E1101
 
 
-class ContinuousRandomVariable(RandomVariable):
+class Multinomial(RandomVariableNode):
+    
+    
+    __slots__ = ['possible_values', 'prior_prob', 'prior_logprob']
+
+    
+    def __init__(self, *args, **kwargs):
+        args, p = args[:-1], args[-1]
+        self.dist = st.multinomial
+        self.params = [1, p]
+        self.prior_prob = p
+        self.prior_logprob = np.log(p)
+        self.possible_values = np.eye(len(p), dtype=np.int)
+        RandomVariableNode.__init__(self, *args, **kwargs)
+
+
+    def reset(self):
+        self.value = np.zeros_like(self.value)
+        self.value[np.argmax(self.params[1])] = 1
+
+
+    def get_outcome_probs(self):
+
+        curr_val = self.value
+        
+        logpr = np.zeros_like(curr_val, dtype=np.float64)
+        for i, val in enumerate(self.possible_values):
+            self.value = val
+            logpr[i] = self.get_loglikelihood()
+            
+        pr = np.exp(logpr)
+
+        if not pr.any():
+            p = self.params[1]
+        else:
+            norm = pr.sum()
+            p = pr / norm
+
+        self.value = curr_val
+
+        return p
+
+    
+    def get_loglikelihood(self):
+        loglik = 0.
+        for node in self.children:
+            loglik += node.get_loglikelihood()
+        #loglik += self.dist.logpmf(self.value, *self.params)
+        loglik += self.prior_logprob[np.argmax(self.value)] # rely on N = 1
+        
+        return loglik
+
+    
+    def sample(self):
+        p = self.get_outcome_probs()
+        self.value = np.random.multinomial(1, p)
+        self.total_sampled += 1
+
+
+class Beta(RandomVariableNode):
+
+
     __slots__ = ['l_clip', 'r_clip']
 
+
     def __init__(self, *args, **kwargs):
-        self.l_clip = -np.inf
-        self.r_clip = np.inf
-        RandomVariable.__init__(self, *args, **kwargs)
+        args, a, b = args[:-2], args[-2], args[-1]
+        self.dist = st.beta
+        self.params = [a, b]
+        self.l_clip = 0.01
+        self.r_clip = 0.99
+        RandomVariableNode.__init__(self, *args, **kwargs)
 
 
     def proposal_norm(self):
@@ -87,70 +151,4 @@ class ContinuousRandomVariable(RandomVariable):
     def sample(self):
         self.metropolis_hastings()
         #self.sample_from_prior()
-
-
-class Multinomial(RandomVariable):
-    __slots__ = ['possible_values', 'prior_prob', 'prior_logprob']
-
-    def __init__(self, *args, **kwargs):
-        args, p = args[:-1], args[-1]
-        self.dist = st.multinomial
-        self.params = [1, p]
-        self.prior_prob = p
-        self.prior_logprob = np.log(p)
-        self.possible_values = np.eye(len(p), dtype=np.int)
-        RandomVariable.__init__(self, *args, **kwargs)
-
-
-    def reset(self):
-        self.value = np.zeros_like(self.value)
-        self.value[np.argmax(self.params[1])] = 1
-
-
-    def get_outcome_probs(self):
-
-        curr_val = self.value
-        
-        logpr = np.zeros_like(curr_val, dtype=np.float64)
-        for i, val in enumerate(self.possible_values):
-            self.value = val
-            logpr[i] = self.get_loglikelihood()
-            
-        pr = np.exp(logpr)
-
-        if not pr.any():
-            p = self.params[1]
-        else:
-            norm = pr.sum()
-            p = pr / norm
-
-        self.value = curr_val
-
-        return p
-
-    def get_loglikelihood(self):
-        loglik = 0.
-        for node in self.children:
-            loglik += node.get_loglikelihood()
-        #loglik += self.dist.logpmf(self.value, *self.params)
-        loglik += self.prior_logprob[np.argmax(self.value)] # rely on N = 1
-        
-        return loglik
-
-    def sample(self):
-        p = self.get_outcome_probs()
-        self.value = np.random.multinomial(1, p)
-        self.total_sampled += 1
-
-
-class Beta(ContinuousRandomVariable):
-    __slots__ = []
-
-    def __init__(self, *args, **kwargs):
-        args, a, b = args[:-2], args[-2], args[-1]
-        self.dist = st.beta
-        self.params = [a, b]
-        ContinuousRandomVariable.__init__(self, *args, **kwargs)
-        self.l_clip = 0.01
-        self.r_clip = 0.99
 
