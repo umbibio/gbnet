57f2acc648d3f06eef8037cbdeeac1f73b0365c7
Argenis Arriojas <argearriojas@gmail.com>
2018-11-14 11:19:49 -0500

    Fixed Cython setup file to get numpy's include path

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from Cython.Distutils import Extension
 from Cython.Distutils import build_ext
+import numpy
 import cython_gsl
 
 
 extensions = [
-        Extension("cnodes", ["cnodes.pyx"], libraries=['gsl', 'gslcblas']),
-        Extension("cchain", ["cchain.pyx"], libraries=['gsl', 'gslcblas']),
+        Extension("cnodes", ["cnodes.pyx"], libraries=['gsl', 'gslcblas'], include_dirs=[numpy.get_include()]),
+        Extension("cchain", ["cchain.pyx"], libraries=['gsl', 'gslcblas'], include_dirs=[numpy.get_include()]),
         Extension("cbasemodel", ["cbasemodel.pyx"]),
         Extension("cmodels", ["cmodels.pyx"]),
     ]
