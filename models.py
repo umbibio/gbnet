44dced4f836d8dc2533d17943ba39e15111a6017
Argenis Arriojas <argearriojas@gmail.com>
2018-11-18 19:50:20 -0500

    Make Theta parameters a=20, b=1. This shifts distribution to the right, putting more weight to X variables
    Add prior for X, computed from the ratio nDEG/ntotal of its target genes
    
    Only model0 is being tested

--- models.py
+++ models.py
@@ -13,12 +13,18 @@ class ORNORModel_0(BaseModel):
         rels = self.rels
         DEG = self.DEG
 
-        self.rels = rels
+        rels = rels.assign(DEG=[DEG[trg] for trg in rels['trguid']])
 
         # identify TF considered in current network
         X_list = rels['srcuid'].unique()
 
-        noiseNode = Noise('Noise', None, a=0.05, b=0.001)
+        X = pd.Series(X_list, name='srcuid').to_frame()
+        X = X.assign(out_total=[len(rels[rels['srcuid']==src]) for src in X_list])
+        X = X.assign(out_deg=[rels[rels.srcuid==src]['DEG'].abs().sum() for src in X_list])
+        X = X.set_index('srcuid')
+        X = X.assign(prior0=X.apply(lambda r: 0.95 - r['out_deg'] / r['out_total'] * 0.8, axis=1))
+
+        noiseNode = Noise('Noise', None, a=0.005, b=0.0005)
 
         Ynodes = {}
         for trg, val in DEG.items():
@@ -33,8 +39,10 @@ class ORNORModel_0(BaseModel):
 
         Xnodes, Tnodes = {}, {}
         for src in X_list:
-            Xnodes[src] = Multinomial('X', src, np.array([0.70, 0.30]))
-            Tnodes[src] = Beta('T', src, 2, 2, scale=0.1)
+            X0prior = X.loc[src]['prior0']
+            X1prior = 1. - X0prior
+            Xnodes[src] = Multinomial('X', src, np.array([X0prior, X1prior]))
+            Tnodes[src] = Beta('T', src, 20, 1, scale=0.1)
 
         Snodes = {}
         for edg in rels.index:
@@ -70,6 +78,8 @@ class ORNORModel_0(BaseModel):
 
         ents = self.ents
         rels = self.rels
+        DEG = self.DEG
+        rels = rels.assign(DEG=[DEG[trg] for trg in rels['trguid']])
         src_uids = rels.srcuid.unique()
 
         Noiseres = result.loc[[f'Noise_{i}' for i in [0,1]]]
@@ -110,6 +120,9 @@ class ORNORModel_0(BaseModel):
             Tres = Tres.assign(name=list(ents.loc[Tres.index].name))
             Sres = Sres.assign(srcname=list(ents.loc[Sres['srcuid']].name))
             Sres = Sres.assign(trgname=list(ents.loc[Sres['trguid']].name))
+        Sres = Sres.assign(DEG=list(rels['DEG']))
+        Xres = Xres.assign(out_total=[len(rels[rels['srcuid']==src]) for src in src_uids])
+        Xres = Xres.assign(out_deg=[rels[rels.srcuid==src]['DEG'].abs().sum() for src in src_uids])
 
         self.result = {
             'Noise': Noiseres,
@@ -127,12 +140,18 @@ class ORNORModel_1(BaseModel):
         rels = self.rels
         DEG = self.DEG
 
-        self.rels = rels
+        rels = rels.assign(DEG=[DEG[trg] for trg in rels['trguid']])
 
         # identify TF considered in current network
         X_list = rels['srcuid'].unique()
 
-        noiseNode = Noise('Noise', None, a=0.05, b=0.001)
+        X = pd.Series(X_list, name='srcuid').to_frame()
+        X = X.assign(out_total=[len(rels[rels['srcuid']==src]) for src in X_list])
+        X = X.assign(out_deg=[rels[rels.srcuid==src]['DEG'].abs().sum() for src in X_list])
+        X = X.set_index('srcuid')
+        X = X.assign(prior0=X.apply(lambda r: 0.95 - r['out_deg'] / r['out_total'] * 0.8, axis=1))
+
+        noiseNode = Noise('Noise', None, a=0.005, b=0.0005)
 
         Ynodes = {}
         for trg, val in DEG.items():
@@ -147,7 +166,10 @@ class ORNORModel_1(BaseModel):
 
         Xnodes, Tnodes = {}, {}
         for src in X_list:
-            Xnodes[src] = Multinomial('X', src, np.array([0.60, 0.40]))
+            X0prior = X.loc[src]['prior0']
+            X1prior = 1. - X0prior
+            Xnodes[src] = Multinomial('X', src, np.array([X0prior, X1prior]))
+            Tnodes[src] = Beta('T', src, 20, 1, scale=0.1)
 
         Snodes = {}
         for edg in rels.index:
@@ -156,7 +178,12 @@ class ORNORModel_1(BaseModel):
 
             src, trg = edg
             
-            Snodes[edg] = Multinomial('S', edg, np.array([0.3, 0.4, 0.3]))
+            if Ynodes[trg].value[1]:
+                Sprior = np.array([0.2, 0.6, 0.2])
+            else:
+                Sprior = np.array([0.4, 0.2, 0.4])
+            
+            Snodes[edg] = Multinomial('S', edg, Sprior)
             Snodes[edg].children.append(Ynodes[trg])
             
             Xnodes[src].children.append(Ynodes[trg])
@@ -180,6 +207,8 @@ class ORNORModel_1(BaseModel):
 
         ents = self.ents
         rels = self.rels
+        DEG = self.DEG
+        rels = rels.assign(DEG=[DEG[trg] for trg in rels['trguid']])
         src_uids = rels.srcuid.unique()
 
         Noiseres = result.loc[[f'Noise_{i}' for i in [0,1]]]
@@ -223,6 +252,9 @@ class ORNORModel_1(BaseModel):
             Tres = Tres.assign(trgname=list(ents.loc[Tres.trguid].name))
             Sres = Sres.assign(srcname=list(ents.loc[Sres.srcuid].name))
             Sres = Sres.assign(trgname=list(ents.loc[Sres.trguid].name))
+        Sres = Sres.assign(DEG=list(rels['DEG']))
+        Xres = Xres.assign(out_total=[len(rels[rels['srcuid']==src]) for src in src_uids])
+        Xres = Xres.assign(out_deg=[rels[rels.srcuid==src]['DEG'].abs().sum() for src in src_uids])
 
         self.result = {
             'Noise': Noiseres,
@@ -232,107 +264,5 @@ class ORNORModel_1(BaseModel):
         }
 
 
-class ORNORModel_2(BaseModel):
-
-
-    def generate_vars(self):
-
-        rels = self.rels
-        DEG = self.DEG
-
-        self.rels = rels
-
-        # identify TF considered in current network
-        X_list = rels['srcuid'].unique()
-
-        noiseNode = Noise('Noise', None, a=0.05, b=0.001)
-
-        Ynodes = {}
-        for trg, val in DEG.items():
-            obs_val = np.array([0, 0, 0])
-            obs_val[1 + val] = 1
-            y_prob = noiseNode.table[:, np.argwhere(obs_val)[0, 0]]
-
-            Ynodes[trg] = ORNOR_YLikelihood('Y', trg, y_prob, value=obs_val)
-            noiseNode.a.children.append(Ynodes[trg])
-            noiseNode.b.children.append(Ynodes[trg])
-            noiseNode.children.append(Ynodes[trg])
-
-        Xnodes, Tnodes = {}, {}
-        for src in X_list:
-            Xnodes[src] = Multinomial('X', src, np.array([0.60, 0.40]))
-            Tnodes[src] = Beta('T', src, 1, 1, value=1.0, scale=0.1) # do not sample
-
-        Snodes = {}
-        for edg in rels.index:
-
-            src, trg = edg
-            
-            Snodes[edg] = Multinomial('S', edg, np.array([0.3, 0.4, 0.3]))
-            Snodes[edg].children.append(Ynodes[trg])
-            
-            Xnodes[src].children.append(Ynodes[trg])
-            Tnodes[src].children.append(Ynodes[trg])
-            
-            Ynodes[trg].parents.append(Xnodes[src])
-            Ynodes[trg].parents.append(Tnodes[src])
-            Ynodes[trg].parents.append(Snodes[edg])
-            
-            Ynodes[trg].in_edges.append([Xnodes[src], Tnodes[src], Snodes[edg]])
-
-        self.vars['Noise'] = {0: noiseNode}
-        self.vars['X'] = Xnodes
-        self.vars['S'] = Snodes
-        #self.vars['T'] = Tnodes
-
-
-    def update_result(self, Xgt=None):
-
-        result = self.get_trace_stats(combine=True).drop(columns=['N', 'sum1', 'sum2', 'var'])
-
-        ents = self.ents
-        rels = self.rels
-        src_uids = rels.srcuid.unique()
-
-        Noiseres = result.loc[[f'Noise_{i}' for i in [0,1]]]
-
-        Xres = result.loc[[f'X__{src}_1' for src in src_uids]]
-        Xres = Xres.assign(srcuid=src_uids)
-        Xres['pred'] = Xres.apply(lambda r: 1 if r['mean']>0.5 else 0, axis =1)
-        Xres = Xres.assign(idx=[f'X__{src}' for src in src_uids])
-        Xres = Xres.set_index('idx')
-        if Xgt is not None:
-            Xres = Xres.assign(gt=[Xgt[src] for src in src_uids])
-
-        Sres0 = result.loc[[f'S__{edge}_0' for edge in rels.index]]['mean'].to_frame('S-')
-        Sres0 = Sres0.assign(edge=rels.index)
-        Sres0 = Sres0.set_index('edge')
-        Sres2 = result.loc[[f'S__{edge}_2' for edge in rels.index]]['mean'].to_frame('S+')
-        Sres2 = Sres2.assign(edge=rels.index)
-        Sres2 = Sres2.set_index('edge')
-
-        Sres = pd.concat([Sres0, Sres2], sort=False, axis=1)
-        Sres = Sres.assign(srcuid=list(rels['srcuid']))
-        Sres = Sres.assign(trguid=list(rels['trguid']))
-        Sres = Sres.assign(pred=Sres.apply(lambda r: 1 if r['S+']>0.5 else (-1 if r['S-']>0.5 else 0), axis=1))
-        deg = pd.Series(self.DEG)
-        Sres.assign(DEG=list(deg.loc[Sres['trguid']]))
-        try:
-            Sres = Sres.assign(gt=list(rels['val']))
-        except KeyError:
-            pass
-
-        if ents is not None:
-            Xres = Xres.assign(name=list(ents.loc[Xres.srcuid].name))
-            Sres = Sres.assign(srcname=list(ents.loc[Sres.srcuid].name))
-            Sres = Sres.assign(trgname=list(ents.loc[Sres.trguid].name))
-
-        self.result = {
-            'Noise': Noiseres,
-            'X': Xres,
-            'S': Sres,
-        }
-
-
-class ORNORModel(ORNORModel_1):
+class ORNORModel(ORNORModel_0):
     pass
